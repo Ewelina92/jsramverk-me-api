0c019c6864e06c63fca930661c7d6b93400c7514
Ewelina92 <ewelina.jankowska@outlook.com>
2021-09-11 14:14:33 +0200

    fix cors origin to student server

--- server.js
+++ server.js
@@ -4,25 +4,18 @@ const server = require("http").createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server, {
     cors: {
-        origin: "http://192.168.86.247:3000",
+        origin: "http://www.student.bth.se",
         methods: ["GET", "POST"]
     },
 });
 
 io.on('connection', (socket) => {
     socket.on('open', function (document) {
-        console.log(socket.rooms);
         socket.join(document);
-        console.log("User connected", document);
     });
 
     socket.on("doc_content", function (delta) {
-        // console.log(delta);
-        // console.log(socket.rooms);
-        // console.log(Array.from(socket.rooms)[1]);
         const room = Array.from(socket.rooms)[1];
-
-        console.log("sending to", room);
 
         socket.to(room).emit("doc_content", delta);
     });
